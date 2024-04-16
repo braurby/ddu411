1142504a1c04408737e6aad4311eb305e6b608fa
braurby <andrewkby@gmail.com>
2024-04-16 17:17:24 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -15,69 +15,69 @@ jQuery(function($) {
         $('#myCarousel').find('.carousel-item').first().addClass('active');
 
         var width = $(window).width();
-          if (width >= 1200){
+        if (width >= 1200) {
             $('.navbar-nav li.dropdown').hover(function() {
                 $(this).children('div.dropdown-menu').addClass('show');
             }, function() {
                 $(this).children('div.dropdown-menu').removeClass('show');
             });
-          }
+        }
 
-        $( '.dropdown-menu:not(:has(.dropdown-item))' ).addClass('hidden');
-        $( 'li.dropdown:not(:has(.dropdown-item))' ).find('span.dropdown-toggle').addClass('hidden');
+        $('.dropdown-menu:not(:has(.dropdown-item))').addClass('hidden');
+        $('li.dropdown:not(:has(.dropdown-item))').find('span.dropdown-toggle').addClass('hidden');
 
         $('.owl-carousel').owlCarousel({
-            loop:true,
-            autoWidth:true,
-            items:10,
-            margin:10,
-            responsiveClass:true,
-            responsive:{
-                0:{
-                    items:1,
-                    nav:true
+            loop: true,
+            autoWidth: true,
+            items: 10,
+            margin: 10,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    nav: true
                 },
-                600:{
-                    items:3,
-                    nav:true
+                600: {
+                    items: 3,
+                    nav: true
                 },
-                1000:{
-                    items:5,
-                    nav:true,
-                    loop:false
+                1000: {
+                    items: 5,
+                    nav: true,
+                    loop: false
                 }
             }
         });
 
-		$("#switch_visibility").click(function(){
-			$("body").css({
-				"font-size": "1.5rem",
-				"color": "#000",
-				"background-color": "#fff"
-			});
-		});
+        $("#switch_visibility").click(function() {
+            $("body").css({
+                "font-size": "1.5rem",
+                "color": "#000",
+                "background-color": "#fff"
+            });
+        });
     });
 });
 
 const cookieBox = document.querySelector(".cookie-popup"),
-  buttons = document.querySelectorAll(".cookie-button");
+    buttons = document.querySelectorAll(".cookie-button");
 
 const executeCodes = () => {
-  //if cookie contains codinglab it will be returned and below of this code will not run
-  if (document.cookie.includes("codinglab")) return;
-  cookieBox.classList.add("show");
+    //if cookie contains codinglab it will be returned and below of this code will not run
+    if (document.cookie.includes("codinglab")) return;
+    cookieBox.classList.add("show");
 
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      cookieBox.classList.remove("show");
+    buttons.forEach((button) => {
+        button.addEventListener("click", () => {
+            cookieBox.classList.remove("show");
 
-      //if button has acceptBtn id
-      if (button.id == "acceptBtn") {
-        //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
-        document.cookie = "cookieBy= ddu411; max-age=" + 60 * 60 * 24 * 30;
-      }
+            //if button has acceptBtn id
+            if (button.id == "acceptBtn") {
+                //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+                document.cookie = "cookieBy= ddu411; max-age=" + 60 * 60 * 24 * 30;
+            }
+        });
     });
-  });
 };
 
 //executeCodes function will be called on webpage load
